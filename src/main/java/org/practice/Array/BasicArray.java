bf0d557d38626511f03eb50b8357ee1f379df638
kamalkings <yakamalkings@gmail.com>
2024-08-03 16:09:38 +0530

    First commit

---
+++ src/main/java/org/practice/Array/BasicArray.java
@@ -0,0 +1,15 @@
+package org.practice.Array;
+
+public class BasicArray {
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Array practice");
+     int arr[] = new int[]{1, 2, 3, 4, 5};
+     for(int i=0;i<5;i++){
+         System.out.print(arr[i]);
+     }
+
+
+    }
+}
